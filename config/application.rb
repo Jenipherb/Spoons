06f4cde241ec7245185781e8b36f9b611be66d2a
Jenipher Belloso <jenipherbelloso@gmail.com>
2022-09-22 11:03:20 -0600

    rails setup

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module Spoons
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+
+    
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -35,5 +38,14 @@ module Spoons
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+      # ▾ Must add these lines! ▾
+    # Adding back cookies and session middleware
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+     # Use SameSite=Strict for all cookies to help protect against CSRF
+     config.action_dispatch.cookies_same_site_protection = :strict
+
   end
 end
