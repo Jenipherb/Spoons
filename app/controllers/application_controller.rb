5b4eb2852f55ef23c91dd582fd10970e4be4078c
Jenipher Belloso <jenipherbelloso@gmail.com>
2022-09-26 14:50:24 -0600

    application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid_response
+  
+    before_action :authorize
+
     def hello_world
         session[:count] = (session[:count] || 0) + 1
         render json: { count: session[:count]}
@@ -9,5 +14,15 @@ class ApplicationController < ActionController::API
     def authorize
         @current_user = User.find_by(id: session[:user_id])
         render json: { error: 'Not Authorized' }, status: :unathorized unless @current_user
+    end
+
+    private
+
+    def record_not_found_response(e)
+      render json: { error: e }, status: 404
+    end
+  
+    def record_invalid_response(e)
+      render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
