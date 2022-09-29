44c116569f5db10cd80adce67c9f9a11dd07110b
Jenipher Belloso <jenipherbelloso@gmail.com>
2022-09-28 18:44:51 -0600

    Home and TaskList added

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,9 +1,12 @@
-
+import TaskList from "./TaskList"
 
 
 function Home(user){
     return(
-        <div>HomePage</div>
+        <div>
+            <h2>Home Page</h2>
+            <TaskList user={user}/>
+        </div>
     )
 }
 
---
+++ client/src/Components/TaskList.js
@@ -0,0 +1,12 @@
+
+
+
+
+function TaskList(user){
+    return(
+        <div></div>
+    )
+}
+
+
+export default TaskList
